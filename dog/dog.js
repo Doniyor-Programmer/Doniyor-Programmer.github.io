560cb6dc2cd52ea22192e1e2af83548da1f016bd
Doniyor-Programmer <ddavronovqasoskor132007@gmail.com>
2022-12-17 19:33:22 +0500

    get dog

---
+++ dog/dog.js
@@ -0,0 +1,29 @@
+const img = document.querySelector("img");
+const tryIt = document.querySelector(".try");
+const a = document.querySelector("a");
+
+let counter = 1;
+
+getResource = async url => {
+  let res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+  }
+
+  return await res.json();
+};
+
+function useEffect() {
+  getResource("https://dog.ceo/api/breeds/image/random").then(data => {
+    const url = data.message;
+
+    img.src = url;
+    a.href = url;
+    a.download = `dog-${counter++}`;
+  });
+}
+
+useEffect();
+
+tryIt.addEventListener("click", useEffect);
