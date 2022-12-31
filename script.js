1ff3453c7584be9024caa6d0b6a3b60288109e77
Doniyor-Programmer <ddavronovqasoskor132007@gmail.com>
2022-12-31 20:52:51 +0500

    released!

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-function a(a){let b=a.trim();return!b}window.addEventListener("load",()=>{document.querySelector("main").style.display="none"}),window.addEventListener("DOMContentLoaded",()=>{let d=document.querySelector(".check"),e=document.querySelector(".error_text"),f=document.querySelectorAll(".reg"),b=document.querySelector(".enter"),g=document.querySelector(".circle"),h=document.querySelector(".user");b.addEventListener("click",()=>{f.forEach((j,f,b)=>{let c=b[0].value.trim(),i=b[1].value.trim();(""==b[f].value||"200715"!=b[2].value||a(b[f].value))&&"enough"!=c.toLowerCase()?(document.querySelector(".no_sound").play(),e.textContent="Invalid Password or Please fill the inputs"):(document.documentElement.scrollTop=0,document.body.scrollTop=0,e.style.display="none",document.querySelector(".yes_sound").play(),d.classList.add("check_active"),g.textContent=`${c.charAt(0)}${i.charAt(0)}`,h.textContent=`${c} ${i}`)})});let c=document.querySelector("#scrollBtn");window.addEventListener("scroll",()=>{document.body.scrollTop>20||document.documentElement.scrollTop>20?c.style.display="block":c.style.display="none"}),c.addEventListener("click",function(){document.body.scrollTop=0,document.documentElement.scrollTop=0})})
+function a(a) {
+  let b = a.trim();
+  return !b;
+}
+
+function $(selector) {
+  const elem = document.querySelector(selector);
+  return elem;
+}
+
+function $_(selector) {
+  const elems = document.querySelectorAll(selector);
+  return elems;
+}
+
+window.addEventListener("load", () => {
+  $("main").style.display = "none";
+}),
+  window.addEventListener("DOMContentLoaded", () => {
+    let d = $(".check"),
+      e = $(".error_text"),
+      f = $_(".reg"),
+      b = $(".enter"),
+      g = $(".circle"),
+      h = $(".user"),
+      skip = $(".skip");
+
+    function doSmth() {
+      document.documentElement.scrollTop = 0;
+      document.body.scrollTop = 0;
+      e.style.display = "none";
+      $(".yes_sound").play();
+      d.classList.add("check_active");
+    }
+
+    b.addEventListener("click", () => {
+      f.forEach((j, f, b) => {
+        let c = b[0].value.trim(),
+          i = b[1].value.trim();
+
+        if (("" == b[f].value || "200715" != b[2].value || a(b[f].value)) && "enough" != c.toLowerCase()) {
+          $(".no_sound").play();
+          e.textContent = "Invalid Password or Please fill the inputs";
+        } else {
+          doSmth();
+          g.textContent = `${c.charAt(0)}${i.charAt(0)}`;
+          h.textContent = `${c} ${i}`;
+        }
+      });
+    });
+
+    skip.addEventListener("click", () => {
+      doSmth();
+      g.textContent = `NS`;
+      h.textContent = `Name Surname`;
+    });
+
+    let searchInput = $("#search"),
+      ac = $_(".ac");
+
+    searchInput.addEventListener("input", e => {
+      const value = e.target.value.toLowerCase();
+      ac.forEach(item => {
+        const isVisible = item.textContent.toLowerCase().includes(value);
+        if (!isVisible) {
+          item.style.display = "none";
+        } else {
+          item.style.display = "";
+        }
+      });
+    });
+
+    let c = $("#scrollBtn");
+
+    window.addEventListener("scroll", () => {
+      document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? (c.style.display = "block") : (c.style.display = "none");
+    }),
+      c.addEventListener("click", function () {
+        (document.body.scrollTop = 0), (document.documentElement.scrollTop = 0);
+      });
+  });
--- search/search.js
+++ search/search.js
@@ -1,12 +1,12 @@
 const cards = document.querySelectorAll(".card"),
-    searchInput = document.querySelector("input"),
-    header = document.querySelectorAll(".header"),
-    body = document.querySelectorAll(".body");
+  searchInput = document.querySelector("input"),
+  header = document.querySelectorAll(".header"),
+  body = document.querySelectorAll(".body");
 
 searchInput.addEventListener("input", e => {
-    const value = e.target.value.toLowerCase();
-    cards.forEach((card, i) => {
-        const isVisible = header[i].textContent.toLowerCase().includes(value) || body[i].textContent.toLowerCase().includes(value);
-        card.classList.toggle("hide", !isVisible);
-    });
+  const value = e.target.value.toLowerCase();
+  cards.forEach((card, i) => {
+    const isVisible = header[i].textContent.toLowerCase().includes(value) || body[i].textContent.toLowerCase().includes(value);
+    card.classList.toggle("hide", !isVisible);
+  });
 });
